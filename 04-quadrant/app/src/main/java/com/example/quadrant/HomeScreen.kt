1061b9838791ecc3c9c3b74a31f29e382df60a01
PierreVieira <pierrevieiraggg@gmail.com>
2023-06-04 23:50:10 -0300

    remove unnused import

--- 04-quadrant/app/src/main/java/com/example/quadrant/HomeScreen.kt
+++ 04-quadrant/app/src/main/java/com/example/quadrant/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.example.quadrant
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
