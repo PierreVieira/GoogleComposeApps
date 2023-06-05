fbf4144c398a725b756af14963cd54f4eecf4cc3
PierreVieira <pierrevieiraggg@gmail.com>
2023-06-04 22:41:40 -0300

    article app

---
+++ 02-article/app/src/main/java/com/example/article/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.example.article
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.article.ui.theme.ArticleTheme
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(id = R.string.title_text),
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = stringResource(id = R.string.first_paragraph_text))
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = stringResource(id = R.string.second_paragraph_text))
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HomeScreenPreview() {
+    ArticleTheme {
+        HomeScreen()
+    }
+}
