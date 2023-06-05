ccd23ad1e9102d4430015c26bb3e4dd1b89d9b25
PierreVieira <pierrevieiraggg@gmail.com>
2023-06-04 22:43:54 -0300

    justify text

--- 02-article/app/src/main/java/com/example/article/HomeScreen.kt
+++ 02-article/app/src/main/java/com/example/article/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,9 +32,15 @@ fun HomeScreen() {
                 fontSize = 24.sp
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Text(text = stringResource(id = R.string.first_paragraph_text))
+            Text(
+                text = stringResource(id = R.string.first_paragraph_text),
+                textAlign = TextAlign.Justify
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            Text(text = stringResource(id = R.string.second_paragraph_text))
+            Text(
+                text = stringResource(id = R.string.second_paragraph_text),
+                textAlign = TextAlign.Justify
+            )
         }
     }
 }
